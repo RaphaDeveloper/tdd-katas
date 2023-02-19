73418a2a9ded662a12171e0fabedfdf6fecbcd05
Raphael Silva <raphael.silva@siteware.com.br>
2023-02-19 19:24:26 -0300

    Implements scenario when there is no drivers; Implements scenario when there are two drivers and they cross each other once; Implements scenario when there are two drivers and they cross each other more than once; Implements scenario when there as two drivers with different route size but in some moment they cross each other. Because the drivers work 8 hours a day, which is 480 minutes so they repeat the same soute until the end of the day and in some point thet might cross each other. Strategy used in TDD: Triangulation

--- TDD_Katas/GossipingBusDrivers/GossipingBusDrivers/CalculateTheAmountOfStopToExchangeGossip.cs
+++ TDD_Katas/GossipingBusDrivers/GossipingBusDrivers/CalculateTheAmountOfStopToExchangeGossip.cs
@@ -4,6 +4,21 @@
     {
         public string Do(Driver[] drivers)
         {
+            if (!drivers.Any())
+                return "Never";
+
+            Driver driver1 = drivers[0];
+            Driver driver2 = drivers[1];
+
+            for (int min = 0; min < 480; min++)
+            {
+                if (driver1.Route[driver1.CurrentStop] == driver2.Route[driver2.CurrentStop])
+                    return "1";
+
+                driver1.GoToNextStop();
+                driver2.GoToNextStop();
+            }
+
             return "Never";
         }
     }
@@ -12,6 +27,18 @@
     {
         public Driver(int[] route)
         {
+            Route = route;
+        }
+
+        public int[] Route { get; }
+        public int CurrentStop { get; internal set; }
+
+        internal void GoToNextStop()
+        {
+            CurrentStop++;
+
+            if (CurrentStop == Route.Length)
+                CurrentStop = 0;
         }
     }
 }
--- TDD_Katas/GossipingBusDrivers/GossipingBusDriversTests/CalculateTheAmountOfStopToExchangeGossipTest.cs
+++ TDD_Katas/GossipingBusDrivers/GossipingBusDriversTests/CalculateTheAmountOfStopToExchangeGossipTest.cs
@@ -27,6 +27,76 @@ namespace GossipingBusDriversTests
 
 
             result.Should().Be("Never");
+        }        
+
+        [Test]
+        public void Should_Exchange_Gossip_Once_When_There_Is_Two_Drivers_And_They_Cross_Each_Other_Once()
+        {
+            CalculateTheAmountOfStopToExchangeGossip calculateTheAmountOfStopToExchangeGossip = new CalculateTheAmountOfStopToExchangeGossip();
+            Driver driver1 = new Driver(route: new int[] { 1, 2, 3 });
+            Driver driver2 = new Driver(route: new int[] { 1, 4, 5 });
+            Driver[] drivers = new Driver[] 
+            { 
+                driver1,
+                driver2
+            };
+
+
+            string result = calculateTheAmountOfStopToExchangeGossip.Do(drivers);
+
+
+            result.Should().Be("1");
+        }
+
+        [Test]
+        public void Should_Never_Exchange_Gossip_When_There_Is_No_Drivers()
+        {
+            CalculateTheAmountOfStopToExchangeGossip calculateTheAmountOfStopToExchangeGossip = new CalculateTheAmountOfStopToExchangeGossip();
+            Driver[] drivers = new Driver[0];
+
+
+            string result = calculateTheAmountOfStopToExchangeGossip.Do(drivers);
+
+
+            result.Should().Be("Never");
+        }
+
+        [Test]
+        public void Should_Exchange_Gossip_Once_When_There_Is_Two_Drivers_Even_If_They_Cross_Each_More_Than_Once()
+        {
+            CalculateTheAmountOfStopToExchangeGossip calculateTheAmountOfStopToExchangeGossip = new CalculateTheAmountOfStopToExchangeGossip();
+            Driver driver1 = new Driver(route: new int[] { 1, 2, 3 });
+            Driver driver2 = new Driver(route: new int[] { 1, 2, 3 });
+            Driver[] drivers = new Driver[]
+            {
+                driver1,
+                driver2
+            };
+
+
+            string result = calculateTheAmountOfStopToExchangeGossip.Do(drivers);
+
+
+            result.Should().Be("1");
+        }
+
+        [Test]
+        public void Should_Exchange_Gossip_Once_When_There_Is_Two_Drivers_With_Different_Route_Size_But_In_Some_Moment_They_Cross_Each_Other()
+        {
+            CalculateTheAmountOfStopToExchangeGossip calculateTheAmountOfStopToExchangeGossip = new CalculateTheAmountOfStopToExchangeGossip();
+            Driver driver1 = new Driver(route: new int[] { 1, 2, 3 });
+            Driver driver2 = new Driver(route: new int[] { 4, 1 });
+            Driver[] drivers = new Driver[]
+            {
+                driver1,
+                driver2
+            };
+
+
+            string result = calculateTheAmountOfStopToExchangeGossip.Do(drivers);
+
+
+            result.Should().Be("1");
         }
     }
 }
